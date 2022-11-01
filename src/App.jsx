f5317714472c8f2b76819254e1ec885430b2cc9b
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-10-31 21:34:50 -0700

    Build fail. Removing attribution for now

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ const App = () => {
       {/* Thank you state end */}
 
 
-      <div className="attribution">
+      {/* <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge"target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
-      </div>
+      </div> */}
     </div>
   );
 }
