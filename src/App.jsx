ea44b27316c930fc7af69b3fb85723055c4c7edf
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-10-31 21:30:07 -0700

    Change favicon ico

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 
 
       <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge"target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
       </div>
     </div>
