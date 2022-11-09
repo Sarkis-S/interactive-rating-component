408e1f5a1fcda54a166407274ad7f7e009352c42
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-11-08 22:30:53 -0800

    Create Submit and Thank You Card

---
+++ src/components/ThankYouCard.jsx
@@ -0,0 +1,15 @@
+// import mySVG from '../images/illustration-thank-you.svg';
+
+const ThankYouCard = () => {
+  return (
+    <div>
+      <h1>This is the thank you card</h1>
+      {/* <img
+        src={mySVG}
+        alt="triangle with all three sides equal"
+      /> */}
+    </div>
+  )
+}
+
+export default ThankYouCard;
